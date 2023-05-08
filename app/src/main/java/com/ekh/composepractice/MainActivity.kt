eaa113f153af038b1a936e32b41d49aeaefeee8c
EHK00 <pgopose2dev@gmail.com>
2023-05-09 02:01:10 +0900

    todo: state hoisting

--- app/src/main/java/com/ekh/composepractice/MainActivity.kt
+++ app/src/main/java/com/ekh/composepractice/MainActivity.kt
@@ -3,19 +3,7 @@ package com.ekh.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.ekh.composepractice.ui.screens.Practice1
 import com.ekh.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,60 +11,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                MyApp()
+                Practice1()
             }
         }
 
-    }
-}
-
-@Composable
-private fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
-) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name)
-            }
-        }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(
-            modifier = Modifier.padding(24.dp)
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name)
-            }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-private fun DefaultPreview() {
-    ComposePracticeTheme {
-        MyApp()
     }
 }
