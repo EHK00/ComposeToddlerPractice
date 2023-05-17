9aaad8b788702df266f6d8c4d6cb83a7c2f80c72
EHK00 <pgopose2dev@gmail.com>
2023-05-17 20:38:33 +0900

    fix: add pr2

--- app/src/main/java/com/ekh/composepractice/ui/screens/Practice2.kt
+++ app/src/main/java/com/ekh/composepractice/ui/screens/Practice2.kt
@@ -1,2 +1,49 @@
 package com.ekh.composepractice.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ekh.composepractice.R
+import com.ekh.composepractice.ui.theme.ComposePracticeTheme
+
+// https://developer.android.com/codelabs/jetpack-compose-layouts#0
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+            .background(color = MaterialTheme.colorScheme.surface),
+    )
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+private fun Preview() {
+    ComposePracticeTheme {
+        SearchBar()
+    }
+}
